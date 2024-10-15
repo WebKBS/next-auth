73d2a8fed0145f06429a4f7eb1af85c55006bf6e
kbs21c1987 <kbs21c1987@gmail.com>
2024-10-15 20:18:25 +0900

    add: prisma 회원가입 로직 완료

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,8 @@
 
 import { z } from "zod";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcryptjs";
+import db from "@/lib/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -12,7 +14,29 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 
   // await new Promise((resolve) => setTimeout(resolve, 3000));
 
-  console.log(values);
+  const { email, password, name } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return { error: "이미 존재하는 이메일입니다." };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  });
+
+  // TODO: 이메일 확인 보내기
 
   return { success: "전송 완료!" };
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import db from "@/lib/db";
 
 export const metadata: Metadata = {
   title: "Next Auth 만들기",
   description: "Next.js로 인증 시스템 만들기",
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const user = await db.user.findMany();
-  console.log("user", user);
-
   return (
     <html lang="en">
       <body>{children}</body>
