84a7d7110c9406139adde287e19c1de771cfe1f1
kbs21c1987 <kbs21c1987@gmail.com>
2024-10-16 16:58:06 +0900

    add: auth.config 주석 추가

--- auth.config.ts
+++ auth.config.ts
@@ -11,21 +11,28 @@ export default {
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
 
+        // zod 유효성 성공시
         if (validatedFields.success) {
           const { email, password } = validatedFields.data;
 
+          // 이메일로 사용자 찾기
           const user = await getUserByEmail(email);
+
+          // 사용자가 없거나 비밀번호가 없으면 null 반환
           if (!user || !user.password) {
             return null;
           }
 
+          // 비밀번호 일치 여부 확인
           const passwordsMatch = await bcrypt.compare(password, user.password);
 
+          // 비밀번호가 일치하면 사용자 반환
           if (passwordsMatch) {
             return user;
           }
         }
 
+        // 사용자가 없거나 비밀번호가 일치하지 않으면 null 반환
         return null;
       },
     }),
